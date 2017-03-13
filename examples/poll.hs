ed448d99527b569561c85a9c126e13f804213044
kohlrabi <kohlrabi@aragonit>
2017-03-13 22:57:48 +0100

    add example for polling input

---
+++ examples/poll.hs
@@ -0,0 +1,21 @@
+import Sound.RtMidi
+import Numeric
+import Control.Concurrent
+import Control.Monad
+import Foreign.C
+
+mainLoop :: Device -> IO ()
+mainLoop d = do
+  m <- getMessage d
+  if length (fst m) > 0 then
+    putStrLn $ show m
+  else return ()
+
+main = do
+  i <- defaultInput
+  openPort i 0 "RtMidi"
+  id <- forkIO $ forever $ mainLoop i
+  _ <- getLine
+  killThread id
+  closeInput i
+  return ()
